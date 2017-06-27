4c9c5d0bf64d732bccb90e263cac069a5ce1d09a
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-06-27 14:48:34 -0600

    sorts links by popularity and recency. also renders on dom

--- index.js
+++ index.js
@@ -4,26 +4,25 @@
   const testFolder = $('#folder-form')
 
   let foldersArray = [{title: 'yellow', url:'llllll', 'popularity': 0, 'id': 1},
-  {title: 'yellow', url:'llllll', 'popularity': 2, 'folder': 'color', 'id': 3},
-  {title: 'blue', url:'llllll', 'popularity': 4, 'folder': 'color', 'id': 2},
-  {title: 'red', url:'llllll', 'popularity': 1, 'folder': 'color', 'id': 4}]
+  {title: 'yellow', url:'llllll', 'popularity': 2, folder: 'color', 'id': 3},
+  {title: 'blue', url:'llllll', 'popularity': 4, folder: 'color', 'id': 2},
+  {title: 'red', url:'llllll', 'popularity': 1, folder: 'color', 'id': 4}]
 
   const urlList = (title) => {
     const foundFolder = foldersArray.filter((folder) => {
       return folder.folder === title
     })
     console.log(foundFolder);
-    urlLister(foundFolder)
+    return urlLister(foundFolder)
   }
 
   const urlLister = (folderObject) => {
     console.log(folderObject)
-    // const listItem = folderObject.map((object) => {
-    //   return (
-    //     `<li>${object.url, object.title}</li>`
-    //   )
-    // })
-    // return listItem
+    folderObject.forEach((object) => {
+      console.log(object.title)
+      $('.url-list').append(`<li>${object.url}, ${object.title}</li>`)
+      // $('.url-list').append(`<li> ITEM HERE </li>`)
+    })
   }
 
   testFolder
@@ -32,19 +31,25 @@
     //on submit, grab value of folder select and use for server things
 
     $('.folder-title').replaceWith(`<div class = "folder-title"><h2>${e.target.value}</h2></div>`)
-    $('.folder-title').append(`<ul>${urlList(e.target.value)}</ul>`)
+    $('.folder-title').append(`<ul class = 'url-list'></ul>`)
     $('.folder-content').addClass('active')
+    urlList(e.target.value)
   })
   .on('submit', (e) => {
     e.preventDefault();
   })
 
   const folderSorter = $('#folder-sort')
+  const removeUrls = () =>{
+    $('.url-list').empty()
+  }
 
   const sortUrls = (objProperty) => {
     foldersArray.sort((a, b) => {
       return a[objProperty] - b[objProperty]
     })
+    removeUrls()
+    urlLister(foldersArray)
   }
 
   folderSorter
