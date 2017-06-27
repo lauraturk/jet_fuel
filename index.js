e2073320e6f5bd9b69a9e4811c202e25972ca98d
Laura Turk <laura.lk.turk@gmail.com>
2017-06-26 23:02:40 -0600

    Start list sorts

--- index.js
+++ index.js
@@ -3,13 +3,55 @@
 
   const testFolder = $('#folder-form')
 
+  let foldersArray = [{title: 'yellow', url:'llllll', 'popularity': 0, 'id': 1},
+  {title: 'yellow', url:'llllll', 'popularity': 2, 'folder': 'color', 'id': 3},
+  {title: 'blue', url:'llllll', 'popularity': 4, 'folder': 'color', 'id': 2},
+  {title: 'red', url:'llllll', 'popularity': 1, 'folder': 'color', 'id': 4}]
+
+  const urlList = (title) => {
+    const foundFolder = foldersArray.filter((folder) => {
+      return folder.folder === title
+    })
+    console.log(foundFolder);
+    urlLister(foundFolder)
+  }
+
+  const urlLister = (folderObject) => {
+    console.log(folderObject)
+    // const listItem = folderObject.map((object) => {
+    //   return (
+    //     `<li>${object.url, object.title}</li>`
+    //   )
+    // })
+    // return listItem
+  }
+
   testFolder
   .on('change', (e) => {
     e.preventDefault()
     //on submit, grab value of folder select and use for server things
-    $('.folder-title').replaceWith(`<div class = "folder-title"><h2>${$('.target').val()}</h2></div>`)
+
+    $('.folder-title').replaceWith(`<div class = "folder-title"><h2>${e.target.value}</h2></div>`)
+    $('.folder-title').append(`<ul>${urlList(e.target.value)}</ul>`)
     $('.folder-content').addClass('active')
   })
   .on('submit', (e) => {
     e.preventDefault();
+  })
+
+  const folderSorter = $('#folder-sort')
+
+  const sortUrls = (objProperty) => {
+    foldersArray.sort((a, b) => {
+      return a[objProperty] - b[objProperty]
+    })
+  }
+
+  folderSorter
+  .on('change', (e) => {
+    e.preventDefault()
+    e.target.value === 'popularity' ? sortUrls('popularity') : sortUrls('id')
+
+    console.log('value', e.target.value)
+    console.log('sortedArray', foldersArray)
   })
