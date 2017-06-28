b2a2bd7b763804f29849094e62203e2cca607228
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-06-28 14:49:24 -0600

    add dummy seed data, also one migration to add title column

---
+++ db/seed/dev/folders.js
@@ -0,0 +1,24 @@
+
+exports.seed = (knex, Promise) => {
+  // Deletes ALL existing entries
+  return knex('urls').del()
+    .then(() => knex('folders').del())
+    .then(() =>{
+      return Promise.all([
+        knex('folders').insert({
+          folder_name: 'default'
+        }, 'id')
+        .then(folder =>{
+          return knex('urls').insert({
+            original_url: 'www.idontknowwhattodowithmylife.com',
+            shortened_url: 'www.idk.com',
+            folder_id: folder[0],
+            title: 'I Dont Know What to do with my life',
+          })
+        })
+        .then(() => console.log('Seeding Complete'))
+        .catch((error) => console.log(`Error seeding data: ${error}`))
+      ])
+    })
+    .catch( error => console.log(`Error seeding data: ${error}`))
+};
