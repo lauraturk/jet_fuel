fbb056bf0a0d55a67e6242893619f6f627b9c567
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-06-29 22:14:51 -0600

    a helluva lot of test environment setup

---
+++ test/routes.spec.js
@@ -0,0 +1,16 @@
+process.env.NODE_ENV = 'test';
+
+var chai = require('chai');
+var should = chai.should();
+var chaiHttp = require('chai-http');
+var server = require('../app');
+
+chai.use(chaiHttp);
+
+describe('Client Routes', function() {
+
+});
+
+describe('API Routes', () => {
+
+});
