2ca4427992b4dd8f21b894dc88272c266d5f4e22
Laura Turk <laura.lk.turk@gmail.com>
2017-07-05 09:35:42 -0600

    configure for circle ci

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
 
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/jetfuel_test',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/jetfuel_test',
     useNullAsDefault: true,
     migrations: {
       directory: __dirname +'/db/migrations'
