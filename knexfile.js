6de0afb3eaa1db57891ff875fdbe433bc9fb613e
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-07-05 11:24:54 -0600

    config for staging branch

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,18 @@ module.exports = {
     }
   },
 
+  staging: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/jetfuel_test',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seed/dev'
+    }
+  },
+
   test: {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://localhost/jetfuel_test',
